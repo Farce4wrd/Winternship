a88238fa9113bbc2fbd0beb901ecc0f9a9f7e322
Farce4wrd <ipeterokolocha@gmail.com>
2022-07-20 11:39:17 -0400

    Added company-review-service

---
+++ company-review-service/src/main/java/com/winternship/companyreviewservice/CompanyReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.winternship.companyreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyReviewServiceApplication.class, args);
+	}
+
+}
---
+++ company-review-service/src/test/java/com/winternship/companyreviewservice/CompanyReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winternship.companyreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
